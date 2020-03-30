b3953eb78e85f0c55644e465e40e71bf370858bb
BrunoFerreira10 <brunofelipe.f1@gmail.com>
2020-03-30 18:23:26 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/mun/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/mun/workshopmongodb/config/Instantiation.java
@@ -1,12 +1,15 @@
 package com.mun.workshopmongodb.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mun.workshopmongodb.domain.Post;
 import com.mun.workshopmongodb.domain.User;
+import com.mun.workshopmongodb.repository.PostRepository;
 import com.mun.workshopmongodb.repository.UserRepository;
 
 @Configuration
@@ -14,6 +17,9 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository repository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -24,6 +30,10 @@ public class Instantiation implements CommandLineRunner{
 		repository.deleteAll();
 		repository.saveAll(Arrays.asList(maria,alex,bob));
 		
+		Post post1 = new Post(null, Instant.parse("2018-03-21T09:33:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços.", maria);
+		Post post2 = new Post(null, Instant.parse("2018-03-23T06:52:25Z"), "Bom dia!", "Acordei feliz hoje!", maria);
+		postRepository.deleteAll();
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
